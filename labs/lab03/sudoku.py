f749d0775b7bacd666dc02bb54ffe37e6021a713
Enoch Mwesigwa <47467035+ekm6@users.noreply.github.com>
2020-02-22 03:36:08 -0700

    even more progress

--- labs/lab03/sudoku.py
+++ labs/lab03/sudoku.py
@@ -18,21 +18,21 @@ easy_fig64a = Sudoku('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26
 harder_aima_csp = Sudoku('4173698.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......')
 # Example from http://zonkedyak.blogspot.com/2006/11/worlds-hardest-sudoku-puzzle-al.html
 hardest_escargot = Sudoku('1....7.9..3..2...8..96..5....53..9...1..8...26....4...3......1..4......7..7...3..')
-puzzle = easy_fig64a
+puzzle = harder_aima_csp
 
 print('\nStart:')
 puzzle.display(puzzle.infer_assignment())
 
 # 2. Solve the puzzle.
-tic= time.perf_counter() # start time
+tic= time.perf_counter() # start timer
 
 #depth_first_graph_search(puzzle)
 #AC3(puzzle)
-backtracking_search(puzzle,mrv)
+backtracking_search(puzzle, inference=forward_checking)
 # Consider adding: select_unassigned_variable=mrv & inference=forward_checking
 #min_conflicts(puzzle)
 
-toc= time.perf_counter()
+toc= time.perf_counter() #end timer
 run_time = toc -tic
 
 
